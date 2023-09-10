8da83400f58f9730806f7d50dd4d0881d1391787
Mailson Teles Borges <mailson.borges@projuris.com.br>
2023-09-10 15:50:49 -0500

    Reverting build updating

--- src/parsers/solidity.parser.ts
+++ src/parsers/solidity.parser.ts
@@ -1,19 +1,10 @@
 import { type Parser } from '../models/parser.model';
 import { SOLIDITY_TEMPLATE } from '../templates/solidity.template';
 import { GenericParser } from './generic.parser';
-import * as prettier from 'prettier';
 
 export class SolidityParser extends GenericParser implements Parser {
   parse(contract: string) {
     const content = super.parse(contract, SOLIDITY_TEMPLATE);
-    return prettier.format(content.toString(), {
-      parser: 'solidity-parse',
-      plugins: ['prettier-plugin-solidity'],
-      printWidth: 120,
-      tabWidth: 4,
-      useTabs: false,
-      singleQuote: false,
-      bracketSpacing: false
-    });
+    return content;
   }
 }
